d5a5f2fd0876a6646c15ef4663aae67796ac7dc2
Nicolas Perez <nperez789@gmail.com>
2016-09-09 05:23:52 +0200

    Added option for recursive search in directory

--- search_in_files.py
+++ search_in_files.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """Multithreaded, recursive text or regex search in all text-containing files
-in a directory (pure Python).
+in a directory.
 
 Additional features:
     - Case sensitivity
-    - print results as PrettyTable
+    - Can search recursively in the folder or not
+    - Print results as PrettyTable
         (one table for all files or one table per file)
-    - can also return the result as an OrderedDict 
+    - Can also return the result as an OrderedDict 
 
 Usage examples: 
     - Real example of what I wanted to do when I wrote this program: 
-python search_in_files.py "sleep" -d beloglazov-openstack-neat-a5a853a -fn *.py
+python search_in_files.py "sleep" -d beloglazov-openstack-neat-a5a853a -R -fn *.py
 
     - Other possible examples:
 python search_in_files.py " Watson " -d sherlockHolmesBooks -fn *.txt -s
 python search_in_files.py caseSensitiveWord -C -d ../foldername -fn *.txt
-python search_in_files.py #FIXME -fn *.py
-python search_in_files.py #[A-Z]{3,5} -r -C -fn *.py
+python search_in_files.py #FIXME -R -fn *.py
+python search_in_files.py #[A-Z]{3,5} -r -C -R -fn *.py
 python search_in_files.py "Hello World"
 """
 
@@ -50,7 +51,7 @@ class cd:
 
 
 def search_in_files(expression, isRegex=False, case_sensitive=False,
-    folderpath='.', filename_regex='*',
+    folderpath='.', recursiveSearch=False, filename_regex='*',
     printPrettyTable=True, separate_tables=False):
     
     def select_line_matcher(expression=expression, isRegex=isRegex):
@@ -89,17 +90,19 @@ def search_in_files(expression, isRegex=False, case_sensitive=False,
     #Change current directory to folderpath
     with cd(folderpath):
         #Recursively lists all file paths that match filename_regex
-        filepaths = glob("**/"+filename_regex, recursive=True)
+        filepaths = glob(
+            "**/"+filename_regex if recursiveSearch else filename_regex,
+            recursive=True)
         #Open each file, search for the expression, list all matching lines
         #in order
         filematches = pool.map(search_in_file, filepaths)
         pool.close()
         pool.join()
-        #Map each filepath to its list of matching lines
+        #Map each filepath to its corresponding list of matching lines
         for filepath, filematch in zip(filepaths, filematches):
             results[filepath] = filematch
     
-    #Order the results by file path
+    #Order the results by file path.
     orderedResults = OrderedDict(sorted(
         {k:v for (k,v) in results.items() if len(k.split('\\')) == 1}.items()))
     orderedResultsRest = OrderedDict(sorted(
@@ -120,6 +123,10 @@ def search_in_files(expression, isRegex=False, case_sensitive=False,
                     if matchedLines:
                         print(x)
         else:
+            '''Files located at the root of the directory appear first in the
+            table. Add a blank line between results from different files.
+            Add a separator in the table between files that were found directly
+            in the directory and files that were found recursively.'''
             x = PrettyTable(prettyTableColumns)
             x.align[prettyTableColumns[-1]] = 'l'
             for orderedResultsDict in orderedResultsDicts:
@@ -129,10 +136,17 @@ def search_in_files(expression, isRegex=False, case_sensitive=False,
                             [filepath, matchedLine.num, matchedLine.text])
                     if matchedLines:
                         x.add_row(["", "", ""])
-                if orderedResultsDict != orderedResultsDicts[-1] \
+                if orderedResultsDict is not orderedResultsDicts[-1] \
                     and sum([len(vals) for vals in orderedResultsDict.values()
                             ]) != 0:
                     x.add_row(["-----\n" for _ in range(len(x.field_names))])
+                elif orderedResultsDict is orderedResultsDicts[-1] \
+                    and sum([len(vals) for vals in orderedResultsDict.values()
+                            ]) == 0:
+                    try:
+                        x.del_row(len(x._rows)-1)
+                    except:
+                        pass
             print(x)
     else:
         return orderedResults
@@ -143,26 +157,30 @@ if __name__ == "__main__":
     parser.add_argument("e", type=str, help="the expression to search for")
     parser.add_argument("-r", "--regex", action="store_true",
         help="evaluate the expression as a regex")
+    parser.add_argument("-C", "--casesensitive", action="store_true",
+        help="enable case-sensitive matching")
     parser.add_argument("-d", "--directory", type=str,
         help="the path to the containing directory")
+    parser.add_argument("-R", "--recursive", action="store_true",
+        help="searches recursively in the given directory")
     parser.add_argument("-fn", "--filenameregex", type=str, 
         help="search only in the files whose name match this regex")
     parser.add_argument("-s", "--separatetables", action="store_true",
         help="prints the search results in separate tables for each file")
-    parser.add_argument("-C", "--casesensitive", action="store_true",
-        help="enable case-sensitive matching")
 
     args = parser.parse_args()
     expression = args.e
     isRegex = args.regex
+    case_sensitive = args.casesensitive
     folderpath = args.directory if args.directory else "."
+    recursiveSearch = args.recursive
     filename_regex = args.filenameregex if args.filenameregex else "*"
     separate_tables = args.separatetables
-    case_sensitive = args.casesensitive
 
     search_in_files(expression=expression,
                     isRegex=isRegex,
                     case_sensitive = case_sensitive,
                     folderpath=folderpath,
-                    filename_regex=filename_regex, 
+                    recursiveSearch=recursiveSearch,
+                    filename_regex=filename_regex,
                     separate_tables=separate_tables)
